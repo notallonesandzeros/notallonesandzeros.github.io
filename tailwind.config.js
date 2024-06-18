e1be432b93ff377c4638898ca37ac1d571c7a374
William Osborne <wosborne@enhance.group>
2024-06-18 07:34:29 +0100

    Add mini web app

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,11 @@ module.exports = {
     },
     colors: {
       grey: '#272727',
+      'light-grey': '#4B4B4B',
+      'lighter-grey': '#CECECE',
       white: '#ffffff',
       blue: '#4D92D2',
+      'dark-blue': '#3C4CA0',
       orange: '#D2654D',
       'pale-orange': '#FFEEEA',
       yellow: '#EDB241'
